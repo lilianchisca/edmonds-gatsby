4161803c4f212e8daf80e5a344a707e5e03e6f27
Lilian Chisca <lilian.chisca@gmail.com>
2020-01-27 11:46:32 +0200

    fixed typo in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const wordpressUrl = `http://edmondswp.mydevenv.co/`
+export const wordPressUrl = `http://edmondswp.mydevenv.co/`
 
 export const uploadsUrl = `http://edmondswp.mydevenv.co/wp-content/uploads/`
